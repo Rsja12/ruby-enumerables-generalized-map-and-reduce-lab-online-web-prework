84ed7966798771b46221170040eaba108c4482b1
Alfonso Socarras <asocarras10@gmail.com>
2019-08-30 15:19:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,6 +13,23 @@ end
 
 
 
+def reduce(array, sv=nil)
+  
+  if sv
+    sum = sv
+    i += 0 
+  else
+    sum = array[0]
+    i = 1 
+  end
+  while i < array.length
+   sum = yield(sum, array[i])
+   i += 0 
+  end
+  
+  sum
+end
+
 
 
 
